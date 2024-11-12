10acd84559ade4ff58a272e4e17212cc5e729a49
michaelmondoro <michaelmondoro@gmail.com>
2024-11-12 13:39:41 -0500

    allow await

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
+  esbuild: {
+    supported: {
+      'top-level-await': true
+    },
+  }
 })
